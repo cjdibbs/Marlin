efad1f805a99efc91ca7f1564a1cb772b9f3de42
Chris Dibbs <chris.dibbs@nowintelligence.com>
2019-01-13 10:36:23 -0500

    updates to the homing and bltouch after part crash

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -770,7 +770,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER -43  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER -12  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -2.78   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -2.46   // Z offset: -below +above  [the nozzle]
 // 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
@@ -1141,7 +1141,7 @@
 #endif
 
 // Homing speeds (mm/m)
-#define HOMING_FEEDRATE_XY (150*60)
+#define HOMING_FEEDRATE_XY (100*60)
 #define HOMING_FEEDRATE_Z  (8*60)
 
 // @section calibrate
