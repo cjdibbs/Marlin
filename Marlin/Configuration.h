de28103e1fadfa35cd887ddd939e79849f2aff1d
Chris Dibbs <chris.dibbs@nowintelligence.com>
2019-01-15 11:17:46 -0500

    tweak for new nozzle

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -770,7 +770,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER -43  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER -12  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -2.60   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -2.65   // Z offset: -below +above  [the nozzle]
 // 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
